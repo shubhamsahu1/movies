ffbb2df8f6dec2d672dd68196f89a4743868a32f
Shubham Sahu <shubhamsahu.sahu@gmail.com>
2022-07-16 16:12:48 +0530

    adding Axios

---
+++ src/api/getMovie.js
@@ -0,0 +1,15 @@
+import clientAxios from "./axios/clientAxios";
+
+export const fetchMovies = async (options = {}) => {
+  const {page, searchinput = "DEFAULT"} = options;
+  const apikey = `${process.env.REACT_APP_API_KEY}`;
+
+  /** build url */
+  const url = `/`;
+
+  /** build query */
+  const params = {apikey, page, s: searchinput};
+
+  /** http call */
+  return await clientAxios.get(url, {params});
+};
