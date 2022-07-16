bf5547b6840a376c3189cd70fe289c308bb360f2
Shubham Sahu <shubhamsahu.sahu@gmail.com>
2022-07-16 14:22:59 +0530

    reusable input box

--- src/component/search/search.jsx
+++ src/component/search/search.jsx
@@ -1,10 +1,15 @@
+import {useCallback, useState} from "react";
 import Button from "../../shared/button";
 import InputText from "../../shared/inputText";
 import styles from "./search.module.scss";
 const Search = () => {
+  const [inputVal, setInputVal] = useState("");
+  const onChange = useCallback((e) => {
+    setInputVal(e.target.value);
+  }, []);
   return (
     <div className={styles.container}>
-      <InputText />
+      <InputText value={inputVal} onChange={onChange} />
       <div className={styles.button}>
         <Button>Search</Button>
       </div>
