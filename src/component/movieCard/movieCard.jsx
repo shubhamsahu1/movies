fa3b5360c00b072531be6b26a1cc54850f320a0d
Shubham Sahu <shubhamsahu.sahu@gmail.com>
2022-07-17 09:34:04 +0530

    adding Image component to movie card

--- src/component/movieCard/movieCard.jsx
+++ src/component/movieCard/movieCard.jsx
@@ -4,7 +4,9 @@ const MovieCard = ({data}) => {
   return (
     <div className={styles.container}>
       <h3>{data.Title}</h3>
-      <Image src={data.Poster} alt={data.Title} />
+      <div className={styles.poster}>
+        <Image src={data.Poster} alt={data.Title} width={300} />
+      </div>
     </div>
   );
 };
